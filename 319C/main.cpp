42781843dc3bd8cb675078f03c3867f039a0da9b
Cameron Frandsen <cfrandsen@sdl.usu.edu>
2020-02-25 19:48:35 -0700

    319c is now working

--- 319C/main.cpp
+++ 319C/main.cpp
@@ -1,41 +1,23 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
-#include <stack>
 #include <vector>
 
 // typedef std::pair<long long, long long> Point;
 struct Tree
 {
-  unsigned long long a;
-  unsigned long long b;
-  unsigned long long dp;
+  long long a;
+  long long b;
+  long long dp;
   Tree() : a(0), b(0), dp(0) {}
-  Tree(unsigned long long a, unsigned long long b) : a(a), b(b), dp(0) {}
-  Tree(unsigned long long a, unsigned long long b, unsigned long long dp)
-    : a(a), b(b), dp(dp)
-  {
-  }
+  Tree(long long a, long long b) : a(a), b(b), dp(0) {}
+  Tree(long long a, long long b, long long dp) : a(a), b(b), dp(dp) {}
 };
 
-Tree operator-(const Tree& a, const Tree& b)
+long long calc(const Tree& p1, const Tree& p2)
 {
-  return {0, a.b - b.b, a.dp - b.dp};
-}
-
-bool isCrossNegative(const Tree& a, const Tree& b)
-{
-  return a.b * b.dp <= a.dp * b.b;
-}
-
-bool isCrossNegative(const Tree& p1, const Tree& p2, const Tree& p3)
-{
-  return isCrossNegative(p2 - p1, p3 - p1);
-}
-
-unsigned long long calc(const Tree& p1, const Tree& p2)
-{
-  return p1.dp + p1.b * p2.a;
+  auto diff = p2.b - p1.b;
+  return (p1.dp - p2.dp + diff - 1) / diff;
 }
 
 int main()
@@ -59,15 +41,14 @@ int main()
   for (int i = 1; i < trees.size(); i++)
   {
     while (inc + 1 < size &&
-           calc(trees[queue[inc + 1]], trees[i]) <=
-             calc(trees[queue[inc]], trees[i]))
+           calc(trees[queue[inc]], trees[queue[inc + 1]]) <= trees[i].a)
       inc++;
 
-    trees[i].dp = calc(trees[queue[inc]], trees[i]);
+    trees[i].dp = trees[i].a * trees[queue[inc]].b + trees[queue[inc]].dp;
 
-    while (
-      size > 1 &&
-      isCrossNegative(trees[i], trees[queue[size - 1]], trees[queue[size - 2]]))
+    while (inc + 1 < size &&
+           calc(trees[queue[size - 2]], trees[queue[size - 1]]) >=
+             calc(trees[queue[size - 1]], trees[i]))
       size--;
 
     if (inc >= size) inc = size - 1;
--- Playground/main.cpp
+++ Playground/main.cpp
@@ -1,40 +1,227 @@
 //#include <bits/stdc++.h>
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <vector>
 
-using namespace std;
+typedef long long ll;
+typedef long double ld;
 
-const int MAXN = 101000;
+namespace his
+{
+  using namespace std;
 
-long long ar1[MAXN], ar2[MAXN], tot[MAXN];
-long long last[MAXN];
+  const int MAXN = 101000;
 
+  long long ar1[MAXN], ar2[MAXN], tot[MAXN];
+  long long last[MAXN];
 
-long long ret(int a, int b) {
-  return (last[a] - last[b] + ar2[b] - ar2[a] - 1) / (ar2[b] - ar2[a]);
+  long long ret(int a, int b)
+  {
+    return (last[a] - last[b] + ar2[b] - ar2[a] - 1) / (ar2[b] - ar2[a]);
+  }
+
+  ll solve(const ll* a, const ll* b, int n)
+  {
+    //std::ofstream fout("his.txt");
+    for (int i = 0; i < n; i++)
+    {
+      ar1[i] = a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+      ar2[i] = b[i];
+    }
+    int inc = 0;
+    int dec = 0;
+    tot[dec++] = 0;
+    for (int i = 1; i < n; i++)
+    {
+      while (dec - inc > 1 && ret(tot[inc], tot[inc + 1]) <= ar1[i])
+      {
+        inc++;
+      }
+      last[i] = ar1[i] * ar2[tot[inc]] + last[tot[inc]];
+
+      while (dec - inc > 1 &&
+             ret(tot[dec - 1], i) <= ret(tot[dec - 2], tot[dec - 1]))
+      {
+        dec--;
+      }
+      //fout << last[i] << std::endl;
+      tot[dec++] = i;
+    }
+    return last[n - 1];
+  }
 }
 
-int main() {
-  int n;
-  cin >> n;
-  for (int i = 0; i < n; i++) {
-    cin >> ar1[i];
-  }
-  for (int i = 0; i < n; i++) {
-    cin >> ar2[i];
-  }
-  int inc = 0;
-  int dec = 0;
-  tot[dec++] = 0;
-  for (int i = 1; i < n; i++) {
-    while (dec - inc > 1 && ret(tot[inc], tot[inc + 1]) <= ar1[i]) {
-      inc++;
+namespace mine
+{
+  struct Tree
+  {
+    unsigned long long a;
+    unsigned long long b;
+    unsigned long long dp;
+    Tree() : a(0), b(0), dp(0) {}
+    Tree(unsigned long long a, unsigned long long b) : a(a), b(b), dp(0) {}
+    Tree(unsigned long long a, unsigned long long b, unsigned long long dp)
+      : a(a), b(b), dp(dp)
+    {
     }
-    last[i] = ar1[i] * ar2[tot[inc]] + last[tot[inc]];
+  };
 
-    while (dec - inc > 1 && ret(tot[dec - 1], i) <= ret(tot[dec - 2], tot[dec - 1])) {
-      dec--;
-    }
-    tot[dec++] = i;
+  Tree operator-(const Tree& a, const Tree& b)
+  {
+    return {0, a.b - b.b, a.dp - b.dp};
   }
-  cout << last[n - 1] << endl;
+
+  bool isCrossNegative(const Tree& a, const Tree& b)
+  {
+    return a.b * b.dp <= a.dp * b.b;
+  }
+
+  bool isCrossNegative(const Tree& p1, const Tree& p2, const Tree& p3)
+  {
+    return isCrossNegative(p2 - p1, p3 - p1);
+  }
+
+  unsigned long long calc(const Tree& p1, const Tree& p2)
+  {
+    return p1.dp + p1.b * p2.a;
+  }
+
+  ll solve(const ll* a, const ll* b, int n)
+  {
+    //std::ofstream fout("mine.txt");
+    std::ios::sync_with_stdio(false);
+
+    int count = n;
+
+    std::vector<Tree> trees(count);
+
+    for (int i = 0; i < count; i++)
+      trees[i].a = a[i];
+    for (int i = 0; i < count; i++)
+      trees[i].b = b[i];
+
+    std::vector<size_t> queue(count);
+    size_t size(1);
+    size_t inc(0);
+    queue[0] = 0;
+    for (int i = 1; i < trees.size(); i++)
+    {
+      while (inc + 1 < size &&
+             calc(trees[queue[inc + 1]], trees[i]) <=
+               calc(trees[queue[inc]], trees[i]))
+        inc++;
+
+      trees[i].dp = calc(trees[queue[inc]], trees[i]);
+
+      while (size > 1 &&
+             isCrossNegative(
+               trees[i], trees[queue[size - 1]], trees[queue[size - 2]]))
+        size--;
+
+      if (inc >= size) inc = size - 1;
+
+      queue[size++] = i;
+      //fout << trees[i].dp << std::endl;
+    }
+    return trees[count - 1].dp;
+  }
+}
+
+void printCase(const ll* a, const ll* b, int n)
+{
+  std::ofstream fout("testcase.txt");
+  fout << "n = " << n << std::endl;
+  for (int i = 0; i < n; ++i)
+  {
+    fout << a[i] << " ";
+  }
+  fout << std::endl;
+  for (int i = 0; i < n; ++i)
+  {
+    fout << b[i] << " ";
+  }
+  fout << std::endl;
+  fout.close();
+}
+
+void runTests()
+{
+  srand(static_cast<unsigned int>(time_t(0)));
+  const int n = 10000;
+  const int maxHeight = 1000000000;
+  const int maxCost = 1000000000;
+  ll* a;
+  ll* b;
+  a = new ll[n];
+  b = new ll[n];
+  for (int test = 0; test < 1000; ++test)
+  {
+    bool* usedA;
+    usedA = new bool[maxHeight + 1];
+    bool* usedB;
+    usedB = new bool[maxCost + 1];
+    memset(usedA, 0, sizeof(usedA));
+    memset(usedB, 0, sizeof(usedB));
+    for (int i = 0; i < n; ++i)
+    {
+      int ax, bx;
+      while (usedA[ax = rand() % (maxHeight - 1) + 2])
+        ;
+      while (usedB[bx = rand() % maxCost + 1])
+        ;
+      a[i] = ax;
+      b[i] = bx;
+      usedA[ax] = true;
+      usedB[bx] = true;
+    }
+    std::sort(a, a + n);
+    std::sort(b, b + n, std::greater<>());
+    a[0] = 1;
+    b[n - 1] = 0;
+    ll ans = mine::solve(a, b, n);
+    ll actual = his::solve(a, b, n);
+    if (ans != actual)
+    {
+      printf("CASE FAILED:\n");
+      printCase(a, b, n);
+      printf("Answer: %lld, Actual: %lld\n", ans, actual);
+      return;
+    }
+    delete[] usedA;
+    delete[] usedB;
+  }
+  delete[] a;
+  delete[] b;
+  printf("All test cases passed.\n");
+}
+
+int main()
+{
+   //runTests();
+
+  std::ifstream fin("testcase.txt");
+
+  int n;
+  fin >> n;
+  ll* a;
+  ll* b;
+  a = new ll[n];
+  b = new ll[n];
+  for (int i = 0; i < n; ++i)
+  {
+    fin >> a[i];
+  }
+  for (int i = 0; i < n; ++i)
+  {
+    fin >> b[i];
+  }
+
+  his::solve(a, b, n);
+  //mine::solve(a, b, n);
+
+  return 0;
 }
